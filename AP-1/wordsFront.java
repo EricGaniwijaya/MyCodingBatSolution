556c916a9faf96755941a527528f3a7fb7beb336
EricGaniwijaya <ericganiwijaya@yahoo.com>
2017-12-10 18:38:54 +0700

    add wordsfront.java

---
+++ AP-1/wordsFront.java
@@ -0,0 +1,9 @@
+public String[] wordsFront(String[] words, int n) {
+  String[] stringArray = new String[n];
+
+  for(int i = 0; i < n; i++) {
+    stringArray[i] = words[i];
+  }
+
+  return stringArray;
+}
