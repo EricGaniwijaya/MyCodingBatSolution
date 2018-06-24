dbe85bde5d2f3f66cccf8d1f42bb1559721b10d4
Eric Ganiwijaya Hasan <EricGaniwijaya@yahoo.com>
2018-06-24 10:14:35 +0700

    add greenticket.java

---
+++ Logic-1/greenTicket.java
@@ -0,0 +1,11 @@
+public int greenTicket(int a, int b, int c) {
+  if (a == b && b == c) {
+    return 20;
+  }
+  
+  if ((a != b && b == c) || (b != c && b == a) || (c != b && a == c)) {
+    return 10;
+  }
+  
+  return 0;
+}
