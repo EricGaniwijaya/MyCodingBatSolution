c3227427b197a4a4000f2643d5f384f62056c754
Eric Ganiwijaya Hasan <EricGaniwijaya@yahoo.com>
2018-07-11 20:52:37 +0700

    add startword.java

---
+++ String-1/startWord.java
@@ -0,0 +1,15 @@
+public String startWord(String str, String word) {
+  if (str.length() < word.length()) {
+    return "";  
+  }
+  
+  if (str.substring(0, word.length()).equals(word)) {
+    return word;
+  }
+  
+  if (str.substring(1, word.length()).equals(word.substring(1, word.length()))) {
+    return str.substring(0, word.length());
+  } 
+  
+  return "";
+}
